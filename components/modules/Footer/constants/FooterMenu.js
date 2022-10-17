76cc9e38adaf69b1d50b836e16d869c94869fa17
Sara Hincapie <shincap8@gmail.com>
2022-10-16 19:16:00 -0500

    First mockup

---
+++ components/modules/Footer/constants/FooterMenu.js
@@ -0,0 +1,20 @@
+const Content = [
+  {
+    title: 'work',
+    path: '/work'
+  },
+  {
+    title: 'services',
+    path: '/services'
+  },
+  {
+    title: 'about',
+    path: '/about'
+  },
+  {
+    title: 'contact',
+    path: '/contact'
+  },
+]
+
+export default Content;
