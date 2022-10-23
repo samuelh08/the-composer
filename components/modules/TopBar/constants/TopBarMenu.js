7947339b76f5ee0110315548eeeba4cbb521566f
Samuel <samuelh08.shm@gmail.com>
2022-10-23 15:06:50 -0500

    Add on hover behavior to items

---
+++ components/modules/TopBar/constants/TopBarMenu.js
@@ -0,0 +1,24 @@
+const Content = [
+  {
+    title: 'home',
+    path: '/',
+  },
+  {
+    title: 'work',
+    path: '/work',
+  },
+  {
+    title: 'services',
+    path: '/services',
+  },
+  {
+    title: 'about',
+    path: '/about',
+  },
+  {
+    title: 'contact',
+    path: '/contact',
+  },
+];
+
+export default Content;
