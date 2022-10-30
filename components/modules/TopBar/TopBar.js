855463aaf254fcdb0b4d094d02f22fc613915010
Samuel <samuelh08.shm@gmail.com>
2022-10-30 16:52:44 -0500

    Add icon on top of links on hover

--- components/modules/TopBar/TopBar.js
+++ components/modules/TopBar/TopBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { AppBar, Box, Grid, Link } from '@mui/material';
+
 import SymbolLogo1 from 'assets/img/EddBeatLogo-01.svg';
 import SymbolLogo2 from 'assets/img/EddBeatLogo-02.svg';
 import TwitterIconGray from 'assets/img/TwitterIconGray.svg';
@@ -11,7 +12,7 @@ import BandcampIconGray from 'assets/img/BandcampIconGray.svg';
 import BandcampIconWhite from 'assets/img/BandcampIconWhite.svg';
 import SoundcloudIconGray from 'assets/img/SoundcloudIconGray.svg';
 import SoundcloudIconWhite from 'assets/img/SoundcloudIconWhite.svg';
-
+import Selected from 'assets/img/SeleccionPaginaBanner-01.svg';
 import TopBarMenu from './constants/TopBarMenu';
 
 const TopBar = () => {
@@ -22,54 +23,71 @@ const TopBar = () => {
       position="sticky"
       sx={{
         backgroundColor: '#000000',
-        height: '64px',
+        height: '71.8px',
         justifyContent: 'center',
       }}
     >
       <Grid container alignItems="center">
         <Grid item xs={4}>
-          <Box display="flex" justifyContent="start">
+          <Box display="flex" justifyContent="start" alignItems="end">
             {TopBarMenu.map((item, i) => {
               return (
-                <Link
-                  href={item.path}
-                  underline="none"
-                  color={activeItem === item.title ? '#FFFFFF' : '#444444'}
-                  fontFamily="Hebden Grotesque"
-                  marginX={2}
+                <Grid
+                  container
                   key={i}
-                  onMouseEnter={() => {
-                    setActiveItem(item.title);
-                  }}
-                  onMouseLeave={() => {
-                    setActiveItem(null);
-                  }}
+                  flexDirection="column"
+                  alignItems="center"
                 >
-                  {item.title}
-                </Link>
+                  <Grid item minHeight={'19px'}>
+                    <div hidden={activeItem !== item.title && 'hidden'}>
+                      <Image key={i} src={Selected} alt="selected" />
+                    </div>
+                  </Grid>
+                  <Grid item>
+                    <Link
+                      href={item.path}
+                      underline="none"
+                      color={activeItem === item.title ? '#FFFFFF' : '#444444'}
+                      fontFamily="Hebden Grotesque"
+                      fontSize={'10.79pt'}
+                      marginX={2}
+                      key={i}
+                      onMouseEnter={() => {
+                        setActiveItem(item.title);
+                      }}
+                      onMouseLeave={() => {
+                        setActiveItem(null);
+                      }}
+                    >
+                      {item.title}
+                    </Link>
+                  </Grid>
+                </Grid>
               );
             })}
           </Box>
         </Grid>
         <Grid item xs={4}>
           <Box display="flex" justifyContent="center">
-            <Image
-              src={onIcon ? SymbolLogo2 : SymbolLogo1}
-              alt="Symbol"
-              onMouseEnter={() => {
-                setOnIcon(true);
-              }}
-              onMouseLeave={() => {
-                setOnIcon(false);
-              }}
-            />
+            <Link href="/">
+              <Image
+                src={onIcon ? SymbolLogo2 : SymbolLogo1}
+                alt="Symbol"
+                onMouseEnter={() => {
+                  setOnIcon(true);
+                }}
+                onMouseLeave={() => {
+                  setOnIcon(false);
+                }}
+              />
+            </Link>
           </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box marginRight={4}>
-            <Grid container spacing={2} justifyContent="end">
+          <Box marginRight={6}>
+            <Grid container spacing={4} justifyContent="end">
               <Grid item>
-                <a href="https://soundcloud.com/eddbeatmusic">
+                <Link href="https://soundcloud.com/eddbeatmusic">
                   <Image
                     src={
                       activeItem === 'soundcloud'
@@ -84,10 +102,10 @@ const TopBar = () => {
                       setActiveItem(null);
                     }}
                   />
-                </a>
+                </Link>
               </Grid>
               <Grid item>
-                <a href="https://bandcamp.com/">
+                <Link href="https://eddbeatmusic.bandcamp.com/">
                   <Image
                     src={
                       activeItem === 'bandcamp'
@@ -102,10 +120,10 @@ const TopBar = () => {
                       setActiveItem(null);
                     }}
                   />
-                </a>
+                </Link>
               </Grid>
               <Grid item>
-                <a href="https://www.instagram.com/eddbeatmusic_/">
+                <Link href="https://www.instagram.com/eddbeatmusic_/">
                   <Image
                     src={
                       activeItem === 'instagram'
@@ -120,10 +138,10 @@ const TopBar = () => {
                       setActiveItem(null);
                     }}
                   />
-                </a>
+                </Link>
               </Grid>
               <Grid item>
-                <a href="https://twitter.com/eddbeatmusic">
+                <Link href="https://twitter.com/eddbeatmusic">
                   <Image
                     src={
                       activeItem === 'twitter'
@@ -138,7 +156,7 @@ const TopBar = () => {
                       setActiveItem(null);
                     }}
                   />
-                </a>
+                </Link>
               </Grid>
             </Grid>
           </Box>
