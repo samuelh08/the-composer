7571b6b0dcebf4c280d07414830934e277ca907b
Samuel <samuelh08.shm@gmail.com>
2022-11-07 18:05:29 -0500

    Add Topbar icons constant, height change behavior

---
+++ components/modules/TopBar/constants/TopBarIcons.js
@@ -0,0 +1,37 @@
+import TwitterIconGray from 'assets/img/TwitterIconGray.svg';
+import TwitterIconWhite from 'assets/img/TwitterIconWhite.svg';
+import InstagramIconGray from 'assets/img/InstagramIconGray.svg';
+import InstagramIconWhite from 'assets/img/InstagramIconWhite.svg';
+import BandcampIconGray from 'assets/img/BandcampIconGray.svg';
+import BandcampIconWhite from 'assets/img/BandcampIconWhite.svg';
+import SoundcloudIconGray from 'assets/img/SoundcloudIconGray.svg';
+import SoundcloudIconWhite from 'assets/img/SoundcloudIconWhite.svg';
+
+const Content = [
+  {
+    title: 'soundcloud',
+    path: 'https://soundcloud.com/eddbeatmusic',
+    grayIcon: SoundcloudIconGray,
+    whiteIcon: SoundcloudIconWhite,
+  },
+  {
+    title: 'bandcamp',
+    path: 'https://eddbeatmusic.bandcamp.com/',
+    grayIcon: BandcampIconGray,
+    whiteIcon: BandcampIconWhite,
+  },
+  {
+    title: 'instagram',
+    path: 'https://www.instagram.com/eddbeatmusic_/',
+    grayIcon: InstagramIconGray,
+    whiteIcon: InstagramIconWhite,
+  },
+  {
+    title: 'twitter',
+    path: 'https://twitter.com/eddbeatmusic',
+    grayIcon: TwitterIconGray,
+    whiteIcon: TwitterIconWhite,
+  },
+];
+
+export default Content;
