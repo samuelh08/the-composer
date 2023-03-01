c45197cddf742446b1f411f910410ca68ecb8b14
Samuel Hincapié Monsalve <82913562+samuelh08@users.noreply.github.com>
2023-02-28 20:53:37 -0500

    Add Lets Talk component (#11)
    * Add Lets Talk component
    
    * Add fill background effect to button
    
    * Add pointer cursor on button

---
+++ components/modules/LetsTalk/LetsTalk.js
@@ -0,0 +1,69 @@
+import { Box, Link, Typography } from '@mui/material';
+import React from 'react';
+
+import background from 'assets/video/1. ALTA CALIDAD BAJO PESO.mp4';
+
+const LetsTalk = () => {
+  return (
+    <Box position="relative" width="100%">
+      <video autoPlay loop muted>
+        <source src={background} type="video/mp4" />
+      </video>
+      <Box
+        position="absolute"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        color="white"
+        maxWidth="60rem"
+        textAlign="center"
+        top="50%"
+        left="50%"
+        sx={{ transform: 'translate(-50%, -50%)' }}
+      >
+        <Typography variant="h1">
+          I’ve been working remotely since before it was cool
+        </Typography>
+        <Box
+          sx={{
+            cursor: 'pointer',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '173.783px',
+            height: '116.7278px',
+            borderRadius: '50%',
+            border: '1px solid white',
+            marginTop: '4rem',
+            ':hover': {
+              '& .MuiTypography-root': { color: 'black' },
+              '& .background': { width: '173.783px', height: '116.7278px' },
+            },
+          }}
+        >
+          <Box
+            className="background"
+            sx={{
+              position: 'absolute',
+              width: 0,
+              height: 0,
+              backgroundImage: 'linear-gradient(white, white)',
+              transition: '0.5s',
+              zIndex: -1,
+              borderRadius: '50%',
+            }}
+          ></Box>
+          <Typography
+            variant="subtitle5"
+            color="white"
+            sx={{ textTransform: 'uppercase', transition: 'color 0.5s' }}
+          >
+            Lets Talk
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default LetsTalk;
