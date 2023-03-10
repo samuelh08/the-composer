e1bc24b57a5490e71078d9ef8f9a627a86b22114
Sara Hincapie <shincap8@gmail.com>
2023-03-09 23:22:29 -0500

    Rename Polarity's assets and create Polarity's object

---
+++ pages/work/constants/PolarityObject.js
@@ -0,0 +1,71 @@
+import Banner from "assets/Portfolio/PolarityPuzzles/PolarityImage.png";
+import image from "assets/Portfolio/PolarityPuzzles/PolarityIcon.png"
+//import Short from "assets/Portfolio/PolarityPuzzles/Polarity.mp4"
+import Sample from "assets/Portfolio/PolarityPuzzles/PolarityMuestra.mp3"
+
+
+const PolarityObject = {
+  display:{
+    title: "Polarity",
+    category: 'VIDEOGAMES, SOUND DESIGN',
+    src: image,
+    altText: 'Icon of Polarity',
+    audio: Sample
+  },
+  title: "Polarity",
+  quote: "Traverse a maze full of challenges with your retractable magnet gun in this first person puzzle solving game.",
+  banner: Banner,
+  short: Short,
+  firstSection: {
+    left:[
+      {
+        title: "Purpose",
+        text: "18th ScoreSpace Jam (2022)"
+      },
+      {
+        title: "SERVICES",
+        text: "Music composition & sound design."
+      },
+      {
+        title: "View live site",
+        link: "https://soulexx.itch.io/polarity-puzzles"
+      },
+    ],
+    right: [
+      {
+        title: "The challenge:",
+        text: "Create a track that matches the ambience of the game using mainly synths and drones. It should not distract but at the same time will properly accompany the player through the puzzle solving."
+      },
+      {
+        title: "The concept:",
+        text: "An eeriness should be depicted by the different drones used through the piece, which will be introduced by a heavy bass. Synths with light passages will keep the monotony away and incite some calmness that the puzzle solving is meant to have."
+      },
+    ]
+  },
+  playlist: {
+    title: "Polarity",
+    image,
+    soundcloud: '',
+    bandcamp: '',
+    tracks: [
+      { title: "Polarity", audio: Sample },
+    ],
+  },
+  secondSection: {
+    title: "THE SOUNDTRACK",
+    subtitle: "The inspiration: ",
+    text: "Predominantly from Portal’s original soundtrack, as well as acousmatic music and minimalism, aside from other soundtracks of other puzzle solving games.",
+  },
+  credits: [
+    {
+      title: "Developed by",
+      by: "Soulex"
+    },
+    {
+      title: "Music and audio by",
+      by: "Eduardo Hincapié"
+    },
+  ]
+}
+
+export default PolarityObject;
