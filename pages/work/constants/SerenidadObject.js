74fd90f4248fb266c02954dc9fdd68e55d255b27
Sara Hincapie <shincap8@gmail.com>
2023-03-09 23:24:44 -0500

    Serenidad's object and The sign of the Raven's object

---
+++ pages/work/constants/SerenidadObject.js
@@ -0,0 +1,71 @@
+import Banner from "assets/Portfolio/Serenidad/SerenidadImage.png";
+import image from "assets/Portfolio/Serenidad/SerenidadIcon.png"
+import Short from "assets/Portfolio/Serenidad/Serenidad.mp4"
+import Sample from "assets/Portfolio/Serenidad/SerenidadMuestra.mp3"
+
+
+const SerenidadObject = {
+  display:{
+    title: "Serenidad",
+    category: 'FILM & ANIMATION',
+    src: image,
+    altText: 'Icon of Serenidad',
+    audio: Sample
+  },
+  title: "Serenidad",
+  quote: "As the spring comes, her serenity frames itself…",
+  banner: Banner,
+  short: Short,
+  firstSection: {
+    left:[
+      {
+        title: "Collaboration",
+        text: "Daniela S. Alzate (2021)"
+      },
+      {
+        title: "SERVICES",
+        text: "Music composition"
+      },
+    ],
+    right: [
+      {
+        title: "The challenge:",
+        text: "Properly portray the calm and serenity through an orchestral piece, this time limited to harps, winds and strings."
+      },
+      {
+        title: "The concept:",
+        text: "The String section will create the foundation for the harmony as the harps create the path for the winds to follow, who will sometimes support the strings as well on the harmony."
+      },
+    ]
+  },
+  playlist: {
+    title: "Serenidad",
+    image,
+    soundcloud: '',
+    bandcamp: '',
+    tracks: [
+      { title: "Serenidad", audio: Sample },
+    ],
+  },
+  secondSection: {
+    title: "THE MUSIC",
+    subtitle: "The inspiration: ",
+    text: "Mainly from the orchestral classical ambient genre, as well as the work of Claude Debussy and Jhon Williams.",
+  },
+  credits: [
+    {
+      title: "Illustratrion by",
+      by: "Daniela S. alzate"
+    },
+    {
+      title: "Trailer music Composed by",
+      by: "EDUARDO HINCAPIE "
+    },
+    {
+      title: "Animation by",
+      by: "Juan Pablo Martínez"
+    },
+  ]
+}
+
+export default SerenidadObject;
