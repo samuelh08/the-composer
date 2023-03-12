6b9a4d0d37abe047181f37fbbcfb8b2add88d25d
Samuel Hincapié Monsalve <82913562+samuelh08@users.noreply.github.com>
2023-03-12 18:05:21 -0500

    Add services page (#21)
    * Add services page
    
    * Add bottom margin to scroll for more
    
    * Add small text to services page
    
    * Add margint to bottom of DropDownTable

---
+++ pages/services/DragGalleryObject.js
@@ -0,0 +1,30 @@
+import Image7 from 'assets/pictures/7.png'
+import Image8 from 'assets/pictures/8.png'
+import Image9 from 'assets/pictures/9.png'
+import Image10 from 'assets/pictures/10.png'
+import Image11 from 'assets/pictures/11.png'
+
+const DragList = [
+  {
+    imageSrc: Image7,
+    altText: 'A sketch book and a hand with a pen'
+  },
+  {
+    imageSrc: Image8,
+    altText: 'Eduardo sitting in front of computer'
+  },
+  {
+    imageSrc: Image9,
+    altText: 'A keyboard and a hand touching it'
+  },
+  {
+    imageSrc: Image10,
+    altText: 'Eduardo playing an ocarina in front of a microphone'
+  },
+  {
+    imageSrc: Image11,
+    altText: 'Eduardo with a pen in his hand'
+  }
+]
+
+export default DragList;
