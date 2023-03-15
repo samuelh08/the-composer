f97586e1babd0e48012a2e0be87b86f26d9bd33f
Samuel <samuelh08.shm@gmail.com>
2023-03-14 22:51:04 -0500

    Add reel to home page

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
 import { Box, Grid, Link, Typography } from '@mui/material';
 
@@ -8,13 +8,93 @@ import LetsTalk from 'components/modules/LetsTalk';
 
 import CarouselListGallery from './CarouselListGalery';
 
+import background from 'assets/Portfolio/MainReelTiny.mp4';
+import arrow from 'assets/img/ArrowReel-01.svg';
 import picture from 'assets/pictures/1.png';
 import arrowLink from 'assets/img/ArrowLink-01.svg';
 
 const Home = () => {
+  const [videoClicked, setVideoClicked] = useState(false);
+
+  const handleClick = useCallback(async () => {
+    setVideoClicked((v) => !v);
+    if (!videoClicked) {
+      document.getElementById('reel').currentTime = 0;
+      document.getElementById('reel').pause();
+    }
+  }, [videoClicked]);
+
   return (
     <>
-      <Grid container bgcolor="black" paddingTop="10rem">
+      <Box
+        bgcolor="black"
+        display="flex"
+        justifyContent="center"
+        position="relative"
+        width="100%"
+        onClick={handleClick}
+      >
+        <Box
+          bgcolor={videoClicked ? '' : 'rgba(0 ,0 ,0, 0.4)'}
+          width="100%"
+          height="100%"
+          position="absolute"
+        />
+        <video
+          id="reel"
+          width="100%"
+          autoPlay
+          loop={!videoClicked}
+          muted={!videoClicked}
+          controls={videoClicked}
+        >
+          <source src={background} type="video/mp4" />
+        </video>
+        <Box
+          position="absolute"
+          color="white"
+          maxWidth="60rem"
+          textAlign="start"
+          top="50%"
+          left="50%"
+          sx={{ transform: 'translate(-90%, -50%)' }}
+          display={videoClicked ? 'none' : 'block'}
+        >
+          <Typography variant="h1">Bringing projects to</Typography>
+          <Typography variant="h1">
+            life trought{' '}
+            <span
+              style={{
+                fontStyle: 'italic',
+                textDecoration: 'underline',
+                fontFamily: 'Andale Mono',
+                letterSpacing: 0,
+              }}
+            >
+              Music Concept
+            </span>
+          </Typography>
+        </Box>
+        <Box
+          width="100%"
+          position="absolute"
+          textAlign="center"
+          justifyContent="center"
+          bottom={50}
+          display={videoClicked ? 'none' : 'flex'}
+        >
+          <Typography variant="subtitle4" color="white">
+            SCROLL
+          </Typography>
+          <Box marginX="2rem">
+            <Image src={arrow} alt="arrow" />
+          </Box>
+          <Typography variant="subtitle4" color="white">
+            FOR MORE
+          </Typography>
+        </Box>
+      </Box>
+      <Grid container bgcolor="black" paddingTop="10rem" paddingBottom="5rem">
         <Grid item xs={6} textAlign="end">
           <Typography variant="h3" color="white">
             LATEST
